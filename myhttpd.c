a6fbb0a4f9c660bbae5da6b441964fccb7cce8b5
Valentin Serrago <ogarres@gmail.com>
2013-09-12 13:33:44 -0400

    Add base myhttpd.c

---
+++ myhttpd.c
@@ -0,0 +1,17 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <sys/errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
+
+int main(int argc, char *argv [])
+{
+    exit(0);
+}
