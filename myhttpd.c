7ae69012ca2ec852a58d593b8d46c28afac68540
Valentin Serrago <valentinserrago@gmail.com>
2013-09-15 15:40:31 -0400

    Add param parsing

--- myhttpd.c
+++ myhttpd.c
@@ -6,7 +6,7 @@
 #include <sys/errno.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <strings.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/stat.h>
 
@@ -16,11 +16,30 @@
 
 int main(int argc, char *argv [])
 {
-    int i;  //Loop Var
+    int i, j;  //Loop Variables
 
     for(i=1; i<argc; i++)
     {
         printf("%s\n", argv[i]);
+        if(argv[i][0] == '-')
+        {
+            int len = strlen(argv[i]);
+            for(j=1; j<len; j++)
+            {
+                switch(argv[i][j])
+                {
+                    case 'p': 
+                        printf("p param given\n");
+                        break;
+                    case 'd':
+                        printf("d param given\n");
+                        break;
+                    default:
+                        printf("Unkown param given\n");
+                        break;
+                }
+            }
+        }
     }
     exit(0);
 }
