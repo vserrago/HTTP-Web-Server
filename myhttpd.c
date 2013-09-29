bf85db27b4241efa6871fdbf64ae97ef90004abe
Valentin Serrago <valentinserrago@gmail.com>
2013-09-29 03:26:14 -0400

    Clean up myhttpd.c
    Add comments, format nicely, rearrange, etc

--- myhttpd.c
+++ myhttpd.c
@@ -18,24 +18,25 @@
 #include "util.h"
 
 
-//Server Config Vars
-char* port     = DEFAULTPORT;       //Port to listen in on 
-char* address  = DEFAULTADDRESS;    //Address of server
-char* confname = DEFAULTCONFNAME;   //Name of config file
-
 int main(int argc, char *argv [])
 {
-    int i, j;               //Loop Variables
-    int opt;                //Option element to be returned by getopt
-    int longindex;          //Used by getopt_long
+    //Server Config Vars
+    char* port     = DEFAULTPORT;       //Port to listen in on 
+    char* address  = DEFAULTADDRESS;    //Address of server
+    char* confname = DEFAULTCONFNAME;   //Name of config file
 
     //Server structs
     stserver* serv;                     //Server var
     configuration* config;              //Config struct var
 
+    //Getopt vars
+    int opt;                            //Option element to be returned by getopt
+    int longindex;                      //Used by getopt_long
+
     //Verbose options for getopt
     struct option long_options[] = 
     {
+        //Name      Arg requirement     Flag  Value
         {"debug",   no_argument,        NULL, 'd'},
         {"port",    required_argument,  NULL, 'p'}
     };
@@ -45,10 +46,12 @@ int main(int argc, char *argv [])
     {
         switch(opt)
         {
+            //Debug option
             case 'd':
                 debugflag = 1;
                 servdeblog("Debug mode enabled\n");
                 break;
+            //Port option
             case 'p':
                 port = optarg;
                 int portnum = atoi(port);
@@ -57,7 +60,8 @@ int main(int argc, char *argv [])
                     exiterr("Portnumber is invalid\n");
                 servdeblog("Port flag set with given port %s\n", optarg);
                 break;
-            case '?': //Character for unknown arg
+            //Unkown option
+            case '?': 
             default:
                 //Use exit function as error message is already printed by getopt
                 exit(EXIT_FAILURE);
@@ -72,42 +76,50 @@ int main(int argc, char *argv [])
     serv->address  = cpynewstr(address);
     serv->confname = cpynewstr(confname);
 
-    servdeblog("Server Address: '%s', Port: '%s', Confname: '%s'\n", serv->address, 
-            serv->port, serv->confname);
+    servdeblog("Server Address: '%s', Port: '%s', Confname: '%s'\n", 
+            serv->address, serv->port, serv->confname);
 
-    config = parseconf(confname);   //Parse configuration file and get info from it.
+    //Parse configuration file and get info from it.
+    config = parseconf(confname);
 
-    prepserv(serv); //Create, bind and listen on port
+    //Create, bind and listen on port
+    prepserv(serv); 
 
+    //Connection info structs
     struct sockaddr_storage socket_st;
     socklen_t addr_size = sizeof socket_st;
 
     servlog("Ready to recieve\n");
+
     for(;;) //Forever
     {
+        //Accept connection
         int c = accept(serv->sock, (struct sockaddr *) &socket_st, &addr_size);
 
         if(c < 0)
-        {
             exiterr("Accept error\n");
-        }
 
-        char* reqstr = recievereq(c); //Get resquest in a string format
+        //Get server request in string format
+        char* reqstr = recievereq(c); 
 
+        //Parse server request from string
         request* req = parsereq(reqstr);
 
+        //Generate a response based on the request and its validity
         response* resp = handlereq(req, config);
 
+        //Send the response back to the client
         sendresp(c,resp);
 
-        //TODO free everything
+        //Free connection info
         free(reqstr);
         freereq(req);
         freeresp(resp);
 
         //Close connection
         close(c);
-        break;
+
+        break; //Break for testings sake
     }
 
     //Finish Up
@@ -116,5 +128,7 @@ int main(int argc, char *argv [])
     //Free server structs
     freeconf(config);
     freestserv(serv);
+
+    //Exit
     exit(0);
 }
